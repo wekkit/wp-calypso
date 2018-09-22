a90f2823228c3f71ad1e79512706b724eb5bc5eb
Spen <spen_@hotmail.co.uk>
2018-09-22 20:14:41 +0100

    Importer Store: Cleanup import statements

--- client/lib/importer/actions.js
+++ client/lib/importer/actions.js
@@ -1,17 +1,14 @@
 /** @format */
-
 /**
  * External dependencies
  */
-
 import Dispatcher from 'dispatcher';
 import { castArray, flowRight, includes } from 'lodash';
-import wpLib from 'lib/wp';
-const wpcom = wpLib.undocumented();
 
 /**
  * Internal dependencies
  */
+import wpLib from 'lib/wp';
 import {
 	IMPORTS_AUTHORS_SET_MAPPING,
 	IMPORTS_AUTHORS_START_MAPPING,
@@ -31,7 +28,9 @@ import {
 	IMPORTS_UPLOAD_START,
 } from 'state/action-types';
 import { appStates } from 'state/imports/constants';
-import { fromApi, toApi } from './common';
+import { fromApi, toApi } from 'lib/importer/common';
+
+const wpcom = wpLib.undocumented();
 
 const ID_GENERATOR_PREFIX = 'local-generated-id-';
 
